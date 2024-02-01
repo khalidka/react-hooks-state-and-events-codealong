3f584c67f26478c5b7becd166ce33c570fff38b8
root <root@DESKTOP-3S5SIJL.localdomain>
2024-02-01 23:45:10 +0300

    feat:Add On and Off the button and background-color

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+
 
 function Toggle() {
-  return <button>OFF</button>;
+  const [isOn, setIsOn] = useState(false);
+
+  function handleClick() {
+    setIsOn((isOn) => !isOn);
+  }
+
+  //When the button is ON, let's make the background red and When it's OFF, it should have a white background
+  const color = isOn ? "red" : "white";
+
+  return (
+    <button style={{ background: color }} onClick={handleClick}>
+      {isOn ? "ON" : "OFF"}
+    </button>
+  );
 }
+
+
 
 export default Toggle;
